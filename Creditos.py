73e42731aca8c964a9081fcc5fc2be00924e5192
Joante <33380950+Joante@users.noreply.github.com>
2017-11-07 02:34:20 -0300

    Cambios en credito
    Estoy intentando crear un boton hacia atras que se active con un click, ya logre hacer los creditos faltaria eso nada mas

--- Creditos.py
+++ Creditos.py
@@ -4,22 +4,35 @@ import submenu as dm
 ancho =600
 alto =800
 
+reloj1=pygame.time.Clock()
+volver=pygame.image.load("Imagenes\Volver.jpg")
+class Cursor(pygame.Rect):
+        def __init__(self):
+                pygame.Rect.__init__(self,0,0,1,1)
+        def update(self):
+                self.left,self.top=pygame.mouse.get_pos()
+class Boton (pygame.sprite.Sprite):
+        def __init__ (self,imagen1,x=100,y=700):
+                self.imagen_=imagen1
+                self.rect=self.imagen_.get_rect()
+                self.rect.left,self.rect.top=(x,y)
+                
+cursor1=Cursor()
+boton1=Boton(volver)
+salir = True
+while salir:
+        pygame.init()
+        ventana = pygame.display.set_mode((alto,ancho))
+        fondo=pygame.image.load("imagenes/Fondo_Creditos.jpg").convert_alpha()
+        ventana.blit(fondo, (0, 0))
+        pygame.display.flip()
+        
+        reloj1.tick(20)
+        cursor1.update()
+        boton1.update(ventana,cursor1)
+        pygame.display.update()
+        
 
-pygame.init()
-ventana = pygame.display.set_mode((alto,ancho))
-fondo=pygame.image.load("imagenes/Fondo_Creditos.jpg").convert_alpha()
 
-choose = dm.dumbmenu (ventana,[
-                    'Creadores:',
-           ' Federico Salvatierra',
-           ' Joan Teich',
-           ' Santiago Martinez Vila',
-           ' Mariana Moline',
-           ' Francisco Laperuta'])
-
-
-
-if __name__ == '__main__':
-	sys.stderr.write("You should import me, not start me...")
-	sys.exit()
-pygame.quit()
+quit()
+        
